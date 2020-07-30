5575582e336af7de90098109f2c598c7c32dbe79
FinleyMarshall <Marshallf1129@gmail.com>
2020-07-30 12:38:22 +1200

    database edits

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
+
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+
+router.get('/signup', function(req, res, next) {
+  res.render('signup');
+});
+
 
 module.exports = router;
